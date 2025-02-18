f2c408efbd96e37734f4f6211d00af51f74e653e
klirktag <klirktag@protonmail.com>
2025-02-18 11:01:41 +0100

    add requirements. fix

--- demo1.py
+++ demo1.py
@@ -13,12 +13,14 @@ def class_name(the_object):
     # https://stackoverflow.com/questions/510972/getting-the-class-name-of-an-instance
     return image.__class__.__name__
 
+counter = 0
 for frame in container.decode(video=0):
-    print(frame.index)
+    #print(frame.index)
     image = frame.to_image()
     object_methods = methods_from_object(image)
     print(class_name(image))
     print(object_methods)
     image2 = image.resize((320,200))
     image3 = image2.convert('L')
-    image3.save('output/frame-small-bw-%04d.jpg' % frame.index)
+    image3.save('output/frame-small-bw-%04d.jpg' % counter)
+    counter+=1
